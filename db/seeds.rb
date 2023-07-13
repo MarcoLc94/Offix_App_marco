eb2857aa2247374f4576fc6070fa4edeea3a9143
MarcoLc94 <marcoantoniolopezcastro@hotmail.com>
2023-07-13 11:38:42 -0600

    Add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,50 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Seeding DB"
 Employee.destroy_all
 Department.destroy_all
+engineering = Department.create(name: "Engineering", 
+                               description: "This is a engineering department")
+marketing = Department.create(name: "Marketing", 
+                               description: "This is a marketing department")
+operations = Department.create(name: "Operations", 
+                               description: "This is a operations department")
 
-department = Department.create(name: "Engineering", description:"This is a great department")
-employee = Employee.create(name:"Testino", nationality:"Peru", role:"Software Developer", birth_date: 31.years.ago, department:"engineering") 
+Employee.create(name: "Emmet Bart", 
+                            role: "Software Developer", 
+                            department: engineering, 
+                            nationality: "Peru", 
+                            birth_date: 31.years.ago)
+Employee.create(name: "Alva Galia", 
+                            role: "Senior Software Developer", 
+                            department: engineering, 
+                            nationality: "Mexico", 
+                            birth_date: 35.years.ago)
+Employee.create(name: "Rico Cornelius", 
+                            role: "Apprentice", 
+                            department: engineering, 
+                            nationality: "Peru", 
+                            birth_date: 21.years.ago)
+
+Employee.create(name: "Mario Testino", 
+                role: "Manager", 
+                department: marketing, 
+                nationality: "Bolivia", 
+                birth_date: 30.years.ago)
+Employee.create(name: "Testino Diprueba", 
+                role: "Social Media Creator", 
+                department: marketing, 
+                nationality: "Peru", 
+                birth_date: 25.years.ago)
+
+Employee.create(name: "John Doe", 
+                role: "Operation Manager", 
+                department: operations, 
+                nationality: "Peru", 
+                birth_date: 38.years.ago)
+Employee.create(name: "Johana Doe", 
+                role: "Logistic Supervisor", 
+                department: operations, 
+                nationality: "Bolivia", 
+                birth_date: 27.years.ago)
